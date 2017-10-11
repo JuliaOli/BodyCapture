cd6c81efd2642047a41e0448477060d442af191b
JuliaOli <maju.olivi@gmail.com>
2017-10-11 16:08:29 -0300

    Design
    Signed-off-by: JuliaOli <maju.olivi@gmail.com>

--- CaptureBody-master/CaptureBody/MainWindow.xaml.cs
+++ CaptureBody-master/CaptureBody/MainWindow.xaml.cs
@@ -217,91 +217,6 @@ namespace CaptureBody
 
             }
         }
-        
-        //FUNCTION DIRECT TO NEW CLASSE (CONSTRUCTOR RECIEVE VARIABLE BODY) 
-        /*
-        private void AngleString(StringBuilder csvContent, Body body)
-        {
-            string aux;
-            aux = "Height;" + height.ToString() + "m";
-            csvContent.AppendLine(aux);
-            // Display height.
-
-            //Display Arms
-            aux = "Left;" + left.ToString() + "m";
-            csvContent.AppendLine(aux);
-            aux = "Right;" + right.ToString() + "m";
-            csvContent.AppendLine(aux);
-
-            //tblAngleLeft.Text = "Relative Angle Left: " + leftArmRelativeAngle.ToString() + "º";
-            aux = "Angle Hip Right;" + rightHipAngle.ToString() + " graus";
-            csvContent.AppendLine(aux);
-            
-            //flexion
-            aux = "Right Shoulder Flexion;" + rightShoulderFlexion.ToString() + " graus";
-            csvContent.AppendLine(aux);
-            aux = "Left Shoulder Flexion;" + leftShoulderFlexion.ToString() + " graus";
-            csvContent.AppendLine(aux);
-            
-            //abduction
-            aux = "Right Shoulder Abduction;" + rightShoulderAbduction.ToString() + " graus";
-            csvContent.AppendLine(aux);
-            aux = "Left Shoulder Abduction;" + leftShoulderAbduction.ToString() + " graus";
-            csvContent.AppendLine(aux);
-
-            //Neck
-            aux = "Neck Flexion;" + neckFlexion.ToString() + " graus";
-            csvContent.AppendLine(aux);
-            aux = "Neck Extension;" + neckExtension.ToString() + " graus";
-            csvContent.AppendLine(aux);
-
-            //Display bodyPositions
-            //Display LeftArmPositions
-            try
-            {
-                // FileStream is IDisposable
-                aux = "Position Head X;" + body.Joints[JointType.Head].Position.X.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Head Y;" + body.Joints[JointType.Head].Position.Y.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Head Z," + body.Joints[JointType.Head].Position.Z.ToString();
-                csvContent.AppendLine(aux);
-
-                //Left Shoulder and Elbow Positions
-                aux = "Position Left Shoulder X;" + body.Joints[JointType.ShoulderLeft].Position.X.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Left Shoulder Y;" + body.Joints[JointType.ShoulderLeft].Position.Y.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Left Shoulder Z;" + body.Joints[JointType.ShoulderLeft].Position.Z.ToString();
-                csvContent.AppendLine(aux).ToString();
-                aux = "Position Left Elbow X;" + body.Joints[JointType.ElbowLeft].Position.X.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Left Elbow Y;" + body.Joints[JointType.ElbowLeft].Position.Y.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Left Elbow Z;" + body.Joints[JointType.ElbowLeft].Position.Z.ToString();
-                csvContent.AppendLine(aux);
-                
-                //Right Shoulder and Elbow Positions
-                aux = "Position Right Shoulder Y;" + body.Joints[JointType.ShoulderRight].Position.X.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Right Shoulder X;" + body.Joints[JointType.ShoulderRight].Position.Y.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Right Shoulder Y;" + body.Joints[JointType.ShoulderRight].Position.Z.ToString();
-                csvContent.AppendLine(aux).ToString();
-                aux = "Position Rigth Elbow Y;" + body.Joints[JointType.ElbowRight].Position.X.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Rigth Elbow X;" + body.Joints[JointType.ElbowRight].Position.Y.ToString();
-                csvContent.AppendLine(aux);
-                aux = "Position Rigth Elbow Y;" + body.Joints[JointType.ElbowRight].Position.Z.ToString();
-
-                csvContent.AppendLine(aux);
-            }
-            catch (NullReferenceException)
-            {
-                Debug.WriteLine("Erro loading the file. " + "Variable body hasn't been set yet");
-            }
-            
-        }*/
 
         private void Timer()
         {
@@ -345,128 +260,18 @@ namespace CaptureBody
         {
 
         }
-        
-
-        /*        <Style TargetType="{x:Type Button}" x:Key="StopButton" >
-            <Setter Property="Template">
-                <Setter.Value>
-                    <ControlTemplate TargetType="{x:Type Button}">
-                        <Grid>
-                            <StackPanel Orientation="Horizontal" Background="Transparent">
-                                <TextBlock x:Name="StopText" Text="{TemplateBinding Content}" TextAlignment="Left"  VerticalAlignment="Center" Foreground="{StaticResource KinectPurpleBrush}" FontSize="15" />
-                                <Grid Margin="9,0,0,0">
-                                    <Image x:Name="StopNormal" Source="src\stoppurple.png" Stretch="None" HorizontalAlignment="Center" />
-                                    <Image x:Name="StopHover" Source="src\stopblue.png" Stretch="None" HorizontalAlignment="Center" Visibility="Collapsed" />
-                                </Grid>
-                            </StackPanel>
-                        </Grid>
-                        <ControlTemplate.Triggers>
-                            <Trigger Property="IsMouseOver" Value="true">
-                                <Setter Property="Visibility" Value="Collapsed" TargetName="StopNormal" />
-                                <Setter Property="Visibility" Value="Visible" TargetName="StopHover" />
-                                <Setter Property="Foreground" Value="{StaticResource KinectBlueBrush}" TargetName="StopText" />
-                            </Trigger>
-                        </ControlTemplate.Triggers>
-                    </ControlTemplate>
-                </Setter.Value>
-            </Setter>
-        </Style>*/
-
 
         private void ScreenshotButton_Click(object sender, RoutedEventArgs e)
         {
-            anglesrules.CsvBuilder(body, viewer);
-
-
-
-
-
-            /*
-            StringBuilder csvContent = new StringBuilder();
-            if(body != null && viewer.Camera != null)
+            try
             {
-                csvContent.AppendLine("Type;Angle/Lenght/Position");
-                AngleString(csvContent, body);
-                
-                string time = System.DateTime.UtcNow.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
-                string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                
-                // create a png bitmap encoder which knows how to save a .png file
-                BitmapEncoder encoder = new PngBitmapEncoder();
-                BitmapEncoder encoderCanvas = new PngBitmapEncoder();
-
-                // create frame from the writable bitmap and add to encoder
-                encoder.Frames.Add(BitmapFrame.Create((BitmapSource)viewer.Camera.Source));
-                
-                Int32 width = (Int32)viewer.Skeleton.ActualWidth;
-                Int32 height = (Int32)viewer.Skeleton.ActualHeight;
-                
-                RenderTargetBitmap renderBitmap = new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Pbgra32);
-                renderBitmap.Render(viewer.Skeleton);
-
-                //JpegBitmapEncoder encoder = new JpegBitmapEncoder();
-                PngBitmapEncoder pngEncoder = new PngBitmapEncoder();
-                pngEncoder.Frames.Add(BitmapFrame.Create(renderBitmap));
-                
-                //Create a new folder
-                string folderName = "KinectSaveBodyInformation-BodyIndex-" + time;
-                string subFolder = Path.Combine(myPhotos, folderName);
-
-                try
-                {
-                    // Determine whether the directory exists.
-                    if (Directory.Exists(subFolder))
-                    {
-                        Console.WriteLine("That path exists already.");
-                        return;
-                    }
-
-                    // Try to create the directory.
-                    DirectoryInfo di = Directory.CreateDirectory(subFolder);
-                    Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(subFolder));
-                    
-                }
-                catch (IOException)
-                {
-                    Console.WriteLine("The process failed: {0}", e.ToString());
-                }
-                finally { }
-                
-
-        
-                //Name of the files (csv,png,png(canvas))
-                string path = Path.Combine(subFolder, "KinectSaveBodyInformation-" + time + ".csv");
-
-                //Debug.WriteLine(subFolder);
-                
-                string imagePath = Path.Combine(subFolder, "CameraImage-" + time + ".png");
-
-                //canvas
-
-                string imagePathCanvas = Path.Combine(subFolder, "Skeleton-" + time + ".png");
-
-                Debug.WriteLine(imagePath);
-
-
-                try
-                {
-                    using (FileStream fs = new FileStream(imagePath, FileMode.Create))
-                    {
-                        encoder.Save(fs);
-                    }
-
-                    using (FileStream fs = new FileStream(imagePathCanvas, FileMode.Create))
-                    {
-                        pngEncoder.Save(fs);
-                    }
-
-                    File.AppendAllText(path, csvContent.ToString());
-                }
-                catch (IOException)
-                {
-                    Debug.WriteLine("Erro writing the file");
-                }
-            }*/
+                anglesrules.CsvBuilder(body, viewer);
+            }
+            catch (NullReferenceException)
+            {
+                Debug.WriteLine("Variables wasn't atributed");
+            }
+            
 
         }
 
